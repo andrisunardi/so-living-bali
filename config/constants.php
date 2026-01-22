f454c707f9294efbf9ae6c2a07c7968874074b95
Andri Sunardi <account@andrisunardi.com>
2026-01-22 10:11:13 +0800

    Update Constants Add GHL And Update .env

--- config/constants.php
+++ config/constants.php
@@ -8,5 +8,15 @@ return [
         'channel' => env('SLACK_CHANNEL'),
     ],
 
+    // GHL
+    'ghl' => [
+        'url' => env('GHL_URL'),
+        'client_id' => env('GHL_CLIENT_ID'),
+        'client_secret' => env('GHL_CLIENT_SECRET'),
+        'grant_type' => env('GHL_GRANT_TYPE'),
+        'redirect_uri' => env('GHL_REDIRECT_URI'),
+        'user_type' => env('GHL_USER_TYPE'),
+    ],
+
     'color' => '',
 ];
