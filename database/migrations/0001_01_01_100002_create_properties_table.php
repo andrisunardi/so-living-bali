8748c954de5d3a074f28d83fc3cf64f3afbd0b24
Andri Sunardi <account@andrisunardi.com>
2026-02-04 23:22:00 +0800

    Update Database Migrations Properties Table

--- database/migrations/0001_01_01_100002_create_properties_table.php
+++ database/migrations/0001_01_01_100002_create_properties_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Property\PropertyStatus;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -13,7 +14,66 @@ return new class extends Migration
             $table->id();
             $table->string('code', 10)->unique();
             $table->string('name', 50);
-            $table->string('address', 100);
+            $table->foreignIdFor(User::class)->nullable()->constrained()->nullOnDelete();
+            $table->date('availability_date')->nullable();
+            $table->date('visit_date')->nullable();
+
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->text('address', 200)->nullable();
+            $table->string('area', 50)->nullable();
+
+            $table->integer('land_size')->nullable();
+            $table->integer('building_size_sqm')->unsigned()->nullable();
+            $table->tinyInteger('number_of_floors')->unsigned()->nullable();
+            $table->integer('outdoor_area_size_sqm')->unsigned()->nullable();
+            $table->string('pool_size', 50)->nullable();
+
+            $table->tinyInteger('number_of_bathrooms')->unsigned()->nullable();
+            $table->boolean('ensuite_bathrooms')->unsigned()->default(false);
+            $table->boolean('guest_toilet')->unsigned()->default(false);
+            $table->boolean('storage')->unsigned()->default(false);
+            $table->boolean('living_style')->unsigned()->nullable();
+
+            $table->boolean('full_legal_documentation')->default(false);
+            $table->boolean('fully_furnished')->default(false);
+            $table->boolean('rental_type')->unsigned()->nullable();
+            $table->integer('minimum_rental_duration_months')->unsigned()->nullable();
+            $table->boolean('owner_price_flexibility')->unsigned()->nullable();
+            $table->boolean('price_coherent_with_upper')->default(false);
+
+            $table->boolean('not_directly_exposed_to_main_road')->unsigned()->default(false);
+            $table->boolean('no_festive_venue_nearby')->unsigned()->default(false);
+            $table->boolean('no_ongoing')->unsigned()->default(false);
+            $table->boolean('quiet_access_road')->unsigned()->default(false);
+            $table->boolean('orientation')->unsigned()->nullable();
+            $table->text('view')->nullable();
+
+            $table->boolean('living_area_has_natural_light')->unsigned()->default(false);
+            $table->boolean('bedroom_1_has_natural_light')->unsigned()->default(false);
+            $table->boolean('bedroom_2_has_natural_light')->unsigned()->default(false);
+            $table->text('noise_source_identified')->nullable();
+
+            $table->integer('internet_speedtest_mpbs')->nullable();
+            $table->boolean('power_backup')->unsigned()->nullable();
+            $table->boolean('water_source')->unsigned()->nullable();
+            $table->boolean('electricity')->unsigned()->nullable();
+
+            $table->boolean('eligible_for_upper')->unsigned()->default(false);
+            $table->boolean('eligible_for_premium')->unsigned()->default(false);
+
+            $table->boolean('design_driven_property')->unsigned()->default(false);
+            $table->text('usability_limitations')->nullable();
+
+            $table->boolean('trade_off_identified')->unsigned()->default(false);
+            $table->text('trade_off_description')->nullable();
+            $table->boolean('target_profile')->unsigned()->nullable();
+
+            $table->boolean('operational_risk')->unsigned()->nullable();
+            $table->text('operational_risk_comment')->nullable();
+
+            $table->boolean('status')->unsigned()->default(PropertyStatus::Pending);
+
             $table->foreignIdFor(User::class, 'created_by')->nullable()->constrained('users')->nullOnDelete();
             $table->foreignIdFor(User::class, 'updated_by')->nullable()->constrained('users')->nullOnDelete();
             $table->foreignIdFor(User::class, 'deleted_by')->nullable()->constrained('users')->nullOnDelete();
