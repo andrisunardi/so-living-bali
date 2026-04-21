8859daa9022dc637d7bd92dbf2d0dd7aaf749212
Andri Sunardi <account@andrisunardi.com>
2026-04-21 11:14:43 +0700

    Update Currency Remove BTC Add AUD

--- app/Enums/Currency.php
+++ app/Enums/Currency.php
@@ -6,6 +6,8 @@ enum Currency: string
 {
     case USD = 'usd';
 
+    case AUD = 'aud';
+
     case EUR = 'eur';
 
     case IDR = 'idr';
@@ -16,18 +18,16 @@ enum Currency: string
 
     case YEN = 'yen';
 
-    case BTC = 'btc';
-
     public function icon(): string
     {
         return match ($this) {
             self::USD => 'fas fa-dollar-sign',
+            self::AUD => 'fas fa-dollar-sign',
             self::EUR => 'fas fa-euro-sign',
             self::IDR => 'fas fa-rupiah-sign',
             self::GBP => 'fas fa-sterling-sign',
             self::CNY => 'fas fa-yen-sign',
             self::YEN => 'fas fa-yen-sign',
-            self::BTC => 'fas fa-bitcoin-sign',
         };
     }
 
@@ -35,12 +35,12 @@ enum Currency: string
     {
         return match ($value) {
             self::USD->value => 'fas fa-dollar-sign',
+            self::AUD->value => 'fas fa-dollar-sign',
             self::EUR->value => 'fas fa-euro-sign',
             self::IDR->value => 'fas fa-rupiah-sign',
             self::GBP->value => 'fas fa-sterling-sign',
             self::CNY->value => 'fas fa-yen-sign',
             self::YEN->value => 'fas fa-yen-sign',
-            self::BTC->value => 'fas fa-bitcoin-sign',
         };
     }
 }
