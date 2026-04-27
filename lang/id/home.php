20388c04994d5d50675d4554955cc681c655c368
Andri Sunardi <account@andrisunardi.com>
2026-04-28 03:06:55 +0700

    Update Language

--- lang/id/home.php
+++ lang/id/home.php
@@ -6,6 +6,7 @@ return [
         'title' => 'Hidup Lebih Baik Tinggal Lebih Lama',
         'description' => 'Mulai pengalaman hidup bulanan dan tahunan Anda bersama kami.',
     ],
+
     'form' => [
         'title' => 'Cari rumah jangka panjang',
         'button' => 'Temukan rumah',
@@ -15,6 +16,7 @@ return [
         'type' => 'Semua tipe',
         'price_range' => 'Rentang anggaran',
     ],
+
     'our_values' => [
         'sub_title' => 'Nilai-Nilai Kami',
         'title' => 'Ruang yang Dirancang untuk Hidup, Lebih dari Sekadar Menginap Singkat',
