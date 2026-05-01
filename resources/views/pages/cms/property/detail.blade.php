94db87a65215d44086aeaff1255f522d8bc39022
Andri Sunardi <account@andrisunardi.com>
2026-05-01 07:45:35 +0700

    Update Property Detail Remove Double Status

--- resources/views/pages/cms/property/detail.blade.php
+++ resources/views/pages/cms/property/detail.blade.php
@@ -771,17 +771,6 @@ new #[Title('Detail | Property')] class extends Component {
                     @endforeach
                 </div>
 
-                <div class="row">
-                    <div class="col-sm-5 col-md-4 col-lg-3 col-xl-2">
-                        <div class="fw-bold">{{ trans('property.status') }}</div>
-                    </div>
-                    <div class="col-sm-7 col-md-8 col-lg-9 col-xl-10">
-                        <span class="badge rounded-pill text-bg-{{ $property->status->color() }}">
-                            {{ $property->status->description() }}
-                        </span>
-                    </div>
-                </div>
-
                 <br />
 
                 <div class="row">
