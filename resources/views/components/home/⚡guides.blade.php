8b56815e7002064a11f8f3db03882f24a974375a
Andri Sunardi <account@andrisunardi.com>
2026-09-18 11:27:57 +0700

    Fix Guides

--- resources/views/components/home/⚡guides.blade.php
+++ resources/views/components/home/⚡guides.blade.php
@@ -105,7 +105,7 @@ new #[Lazy] class extends Component {
                         </h1>
 
                         <p class="small">
-                            {{ Str::of($guide->translate_body)->htmlEntityDecode()->stripTags()->limit(100) }}
+                            {{ Str::limit(strip_tags(html_entity_decode($guide->translate_body)), 100) }}
                         </p>
 
                         <a draggable="false" class="text-success" href="{{ route('guide.detail', ['slug' => $guide->slug]) }}"
